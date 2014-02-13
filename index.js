5d942d5bd1e62b878251507c4a2429c486890184
Kenan Yildirim <kenan@kenany.me>
2014-02-13 10:10:10 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var forEach = require('lodash.foreach');
+
+module.exports = function rpn(postfix) {
+  if (postfix.length === 0) {
+    return 0;
+  }
+
+  // Split into array of tokens
+  postfix = postfix.split(/\s+/);
+
+  var stack = [];
+
+  forEach(postfix, function(token) {
+
+    // Token is a value, push it onto the stack
+    if (token == +token) {
+      stack.push(parseFloat(token));
+    }
+
+    // Token is operator
+    else {
+
+      // Every operation requires two arguments
+      if (stack.length < 2) {
+        throw new Error('Insufficient values in expression.');
+      }
+
+      // Pop two items from the top of the stack and push the result of the
+      // operation onto the stack.
+      var y = stack.pop();
+      var x = stack.pop();
+      stack.push(eval(x + token + ' ' + y));
+    }
+  });
+
+  if (stack.length > 1) {
+    throw new Error('Inputted expression has too many values.');
+  }
+
+  return stack.pop();
+}
