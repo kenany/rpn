740a3652e3f6a78f72da11cfeeb553c6ba423d1b
Kenan Yildirim <kenan@kenany.me>
2015-03-25 08:15:04 -0400

    Remove `lodash.foreach` dependency
    Save a function call.
    
    Closes https://github.com/KenanY/rpn/issues/6

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var forEach = require('lodash.foreach');
-
-module.exports = function rpn(postfix) {
+function rpn(postfix) {
   if (postfix.length === 0) {
     return 0;
   }
@@ -10,7 +8,8 @@ module.exports = function rpn(postfix) {
 
   var stack = [];
 
-  forEach(postfix, function(token) {
+  for (var i = 0; i < postfix.length; i++) {
+    var token = postfix[i];
 
     // Token is a value, push it onto the stack
     if (token == +token) {
@@ -31,11 +30,13 @@ module.exports = function rpn(postfix) {
       var x = stack.pop();
       stack.push(eval(x + token + ' ' + y));
     }
-  });
+  }
 
   if (stack.length > 1) {
     throw new Error('Inputted expression has too many values.');
   }
 
   return stack.pop();
-};
+}
+
+module.exports = rpn;
